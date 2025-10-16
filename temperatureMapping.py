bef58ae590a9e3eb5968ba9b0ef1b272bd9f569b
apertureSynthesis <160927686+apertureSynthesis@users.noreply.github.com>
2025-10-16 16:43:05 -0400

    Working through the conversion

--- temperatureMapping.py
+++ temperatureMapping.py
@@ -2,6 +2,7 @@ import os,sys
 sys.path.append(os.environ['HOME']+'/scripts')
 
 from ThermalModel.utils.helpers import getPars
+from ThermalModel.utils.shapes import readPlate
 
 import numpy as np
 
@@ -21,7 +22,15 @@ class temperatureMapping(object):
             #If not, use the tri-axial ellipsoid
             if self.pars['shapeFile'] != 'None':
                 print('Using a shape model: any ellipsoid will be ignored.')
+
+                vertices, triangles = readPlate(self.pars['shapeFile'])
+                print(f"Dimensions of vertices: {sum(len(x) for x in vertices)}, triangles: {sum(len(x) for x in triangles)}")
+
+
+
+
             elif (self.pars['shapeFile'] == 'None') & (self.pars['shape_a'] != 'None'):
                 print('Using an ellipsoid')
             else:
                 raise ValueError('Shape file or ellipsoid missing, but required for temperature profile calculation!')
+            
--- utils/shapes.py
+++ utils/shapes.py
@@ -1,1 +1,47 @@
 import numpy as np
+
+def readPlate(fileName):
+
+    nvert = 0.
+    ntrian = 0.
+
+    x = 0.
+    y = 0.
+    z = 0.
+    i1 = 0.
+    i2 = 0.
+    i3 = 0.
+
+    with open(fileName, 'r') as fn:
+        for line in fn:
+            if len(line.split()) == 2:
+                nvert = line.split[0]
+                ntrian = line.split[1]
+                break
+    
+        vertices = np.zeros((3,nvert))
+        triangles = np.zeros((3,ntrian))
+    
+        i=0
+        for line in fn:
+            if not (line.endswith(']')):
+                break
+            x,y,z1 = line.split()
+            z = z1.split(']')[0]
+            vertices[i,0] = x
+            vertices[i,1] = y
+            vertices[i,2] = z
+            
+            i+=1
+
+        i=0
+        for line in fn:
+            x,y,z = line.split()
+            triangles[i,0] = x
+            triangles[i,1] = y
+            triangles[i,2] = z
+            
+            i+=1
+        
+    return vertices, triangles
+            
